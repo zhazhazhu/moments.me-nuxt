a38d1e1a962d2ed6b7f3b13a5ada09376191b6d9
zhazhazhu <fangyuan.leslie@qq.com>
2024-01-01 01:39:20 +0800

    fix: gitalk body error

--- components/Comments.vue
+++ components/Comments.vue
@@ -5,18 +5,18 @@ const route = useRoute();
 const config = useRuntimeConfig();
 const container = ref<HTMLElement>();
 
-const gitalk = new Gitalk({
-  clientID: config.public.clientID,
-  clientSecret: config.public.clientSecret,
-  repo: "moments.me-nuxt", // The repository of store comments,
-  owner: "zhazhazhu",
-  admin: ["zhazhazhu"],
-  id: route.params.slug as string,
-  distractionFreeMode: false, // Facebook-like distraction free mode
-  title: route.params.slug as string,
-});
-
 onMounted(() => {
+  const gitalk = new Gitalk({
+    clientID: config.public.clientID,
+    clientSecret: config.public.clientSecret,
+    repo: "moments.me-nuxt", // The repository of store comments,
+    owner: "zhazhazhu",
+    admin: ["zhazhazhu"],
+    id: route.params.slug as string,
+    distractionFreeMode: false, // Facebook-like distraction free mode
+    title: route.params.slug[0] as string,
+    body: `地址: ${location.href}`,
+  });
   container.value && gitalk.render(container.value);
 });
 </script>
